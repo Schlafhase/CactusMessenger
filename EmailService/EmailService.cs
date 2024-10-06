a2b907e069264e1167171ba42eab7fbfc7bff4d1
Schlafhase <schlafhase190@gmail.com>
2024-10-06 15:10:59 +0200

    emailaeialmeila

--- EmailService/EmailService.cs
+++ EmailService/EmailService.cs
@@ -4,15 +4,38 @@ namespace EmailService
 {
 	public class EmailService
 	{
-        public EmailService()
-        {
-			SmtpClient mySmtpClient = new SmtpClient("smtpout.secureserver.net");
+		SmtpClient smtpClient;
 
+		public EmailService(string password)
+		{
+			smtpClient = new SmtpClient("smtp.office365.com", 587);
+			smtpClient.EnableSsl = true;
+			//smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
 			// set smtp-client with basicAuthentication
-			mySmtpClient.UseDefaultCredentials = false;
-			System.Net.NetworkCredential basicAuthenticationInfo = new
-			   System.Net.NetworkCredential("", "password");
-			mySmtpClient.Credentials = basicAuthenticationInfo;
+			smtpClient.UseDefaultCredentials = false;
+			System.Net.NetworkCredential basicAuthenticationInfo = new("linus.schneeberg@schlafhase.uk", password);
+			smtpClient.Credentials = basicAuthenticationInfo;
 		}
-    }
+
+		public void Send(string receiver, string subject, string body)
+		{
+			MailAddress from = new MailAddress("linus.schneeberg@schlafhase.uk", "Cactus Messenger");
+			MailAddress to = new MailAddress(receiver);
+			using (MailMessage mail = new(from, to)
+			{
+				// set subject and encoding
+				Subject = subject,
+				SubjectEncoding = System.Text.Encoding.UTF8,
+
+				// set body-message and encoding
+				Body = body,
+				BodyEncoding = System.Text.Encoding.UTF8,
+				// text or html
+				IsBodyHtml = true
+			})
+			{
+				smtpClient.Send(mail);
+			}
+		}
+	}
 }
