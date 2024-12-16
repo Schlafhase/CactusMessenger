4d424a7a3d5a2fd0b27a07ebf28867fc9815f170
Schlafhase <schlafhase190@gmail.com>
2024-12-16 20:06:52 +0100

    Added my image compression library to the Messenger solution

---
+++ OptimisedImages/DeflateCompression.cs
@@ -0,0 +1,51 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace OptimisedImages;
+
+internal class DeflateCompression
+{
+	public static void CopyTo(Stream src, Stream dest)
+	{
+		byte[] bytes = new byte[4096];
+
+		int cnt;
+
+		while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+		{
+			dest.Write(bytes, 0, cnt);
+		}
+	}
+
+	public static byte[] Zip(string str)
+	{
+		byte[]? bytes = Encoding.UTF8.GetBytes(str);
+
+		using (MemoryStream? msi = new(bytes))
+		using (MemoryStream? mso = new())
+		{
+			using (GZipStream? gs = new(mso, CompressionMode.Compress))
+			{
+				//msi.CopyTo(gs);
+				CopyTo(msi, gs);
+			}
+
+			return mso.ToArray();
+		}
+	}
+
+	public static string Unzip(byte[] bytes)
+	{
+		using (MemoryStream? msi = new(bytes))
+		using (MemoryStream? mso = new())
+		{
+			using (GZipStream? gs = new(msi, CompressionMode.Decompress))
+			{
+				//gs.CopyTo(mso);
+				CopyTo(gs, mso);
+			}
+
+			return Encoding.UTF8.GetString(mso.ToArray());
+		}
+	}
+}
